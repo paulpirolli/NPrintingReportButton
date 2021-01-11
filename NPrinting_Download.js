52151abcf5b48672b239f5d48505d13926ce696b
Paul Pirolli <ppirolli@nuwavesolutions.com>
2021-01-11 13:33:13 -0500

    Updated for sorting array, removed onClick from html

--- NPrinting_Download.js
+++ NPrinting_Download.js
@@ -1,7 +1,5 @@
 define( ["qlik", "text!./template.html","css!./styles.css"],
 	function ( qlik, template ) {
-		var hostURLName = location.origin;
-		
 		return {
 			template: template,
 			support: {
@@ -14,49 +12,46 @@ define( ["qlik", "text!./template.html","css!./styles.css"],
 			},
 			controller: ['$scope', function ( $scope ) {
 				//add your rendering code here
-				NPrintButton.onclick = function retrieveReport(){ $.ajax({
-					url: "/qps/user?xrfkey=GAMG717cpRsrx7xR",
-					type: "GET",
-					headers: {
-						"X-Qlik-XrfKey":"GAMG717cpRsrx7xR"
-					},
-					success: function (user){
-						//console.log(user)
-						
-						//get library
-						$.ajax({
-							url: "/qrs/sharedcontent/full?xrfkey=GAMG717cpRsrx7xR&filter=owner.name eq '" + user.userName + "'",
-							type: "GET",
-							headers: {
-								"X-Qlik-XrfKey":"GAMG717cpRsrx7xR"
-							},
-							success: function (sharedContent){
-								
-								//console.log(sharedContent);
-								var nPrintingEndpoint = [];
-								for(i=0;i < sharedContent[0].references.length;i++){
-									//console.log(i);
-									nPrintingEndpoint.push(sharedContent[0].references[i].externalPath);
+				NPrintButton.onclick = function retrieveReport(){ 
+					$.ajax({
+						url: "/qps/user?xrfkey=GAMG717cpRsrx7xR",
+						type: "GET",
+						headers: {
+							"X-Qlik-XrfKey":"GAMG717cpRsrx7xR"
+						},
+						success: function (user){
+							console.log(user)
+							
+							//get library
+							$.ajax({
+								url: "/qrs/sharedcontent/full?xrfkey=GAMG717cpRsrx7xR&filter=owner.name eq '" + user.userName + "'",
+								type: "GET",
+								headers: {
+									"X-Qlik-XrfKey":"GAMG717cpRsrx7xR"
+								},
+								success: function (sharedContent){
+									
+									console.log(sharedContent);
+									var nPrintingEndpoint = [];
+									for(i=0;i < sharedContent[0].references.length;i++){
+										//console.log(i);
+										nPrintingEndpoint.push(sharedContent[0].references[i].externalPath);
+									}
+									//console.log(nPrintingEndpoint);
+									nPrintingEndpoint.reverse();
+									window.open(nPrintingEndpoint[0], '_blank');
+								},
+								error: function (error){
+									console.log(error)
 								}
-								//console.log(nPrintingEndpoint);
-								window.open(nPrintingEndpoint[0], '_blank');
-							},
-							error: function (error){
-								console.log(error)
-							}
-						});
-					},
-					error: function (error){
-						console.log(error)
-					}
-				});
+							});
+						},
+						error: function (error){
+							console.log(error)
+						}
+					});
 				}
 			}]
 		};
 
 	} );
-
-
-
-
-					//url: "https://ec2-34-225-54-124.compute-1.amazonaws.com/qrs/sharedcontent/full?xrfkey=GAMG717cpRsrx7xR&filter=owner.name eq 'ppirolli'",//&filter=name
